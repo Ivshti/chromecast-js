7e94d9723c0959c8795c878e4a8d69191f465f90
ddaf <ddaf@popcorntime.io>
2015-01-08 14:11:27 +0100

    Major refactoring

--- index.js
+++ index.js
@@ -1,32 +1,67 @@
-var util = require( 'util' );
-var events = require( 'events' );
-var mdns = require('mdns-js2');
-var Device = require('./device').Device
-var debug = require('debug')('chromecast-js')
+var util = require('util');
+var events = require('events');
+var mdns = require('mdns-js');
+var Device = require('./device').Device;
+var debug = require('debug')('chromecast-js');
 
-var Browser = function( options ) {
-  events.EventEmitter.call( this );
-  this.init( options );
+var browser = function(options) {
+	events.EventEmitter.call(this);
+	this.init(options);
 };
 
-util.inherits( Browser, events.EventEmitter );
+util.inherits( browser, events.EventEmitter );
+exports.Browser = browser;
 
-exports.Browser = Browser;
+browser.prototype.init = function( options ) {
+	var self = this;
+	this.devices = {};
 
+	this.getDeviceTxtValue = function(device, key, fallback) {
+		var value = fallback !== undefined ? fallback : '';
+		if (device.txt !== undefined) {
+			for (var i = 0; i < device.txt.length; i++) {
+				var keyValue = device.txt[i].split('=');
+				if (keyValue[0] === key) {
+					value = keyValue[1];
+				}
+			}
+		}
+		return value;
+	}
 
-Browser.prototype.init = function( options ) {
-  var self = this
+	var discoverTimeout = 12000;
+	var browser = mdns.createBrowser(mdns.tcp('googlecast'));
 
-  var mdnsBrowser = new mdns.Mdns(mdns.tcp('googlecast'));
+	browser.on('ready', function () {
+		browser.discover();
+	});
 
-  mdnsBrowser.on('ready', function () {
-      mdnsBrowser.discover();
-  });
+	browser.on('update', function (device) {
 
-  mdnsBrowser.on('update', function (device) {
-      var dev_config = {addresses: device.addresses, name: device.name}
-      self.device = new Device(dev_config)
-      self.emit('deviceOn', self.device)
-  });
+		// New mDNS-js (0.2.2) version scans all available network interfaces.
+		// Some naughty devices or virtual networks will respond even though they are not a chromecast device, so we have to check type.
+		// Each chromecast device could respond on more than one interface, so we make sure to only discover each unique device once.
+		// For id, try to use 'id' from mDNS response txt part, but use ip address if not present.
+		// For device name, try to use 'fn' from mDNS response txt part, but use hostname if not present.
 
-}
+		if (device.type !== undefined && device.type.length > 0 && device.type[0].name === 'googlecast') {
+
+			var deviceId = self.getDeviceTxtValue(device, 'id', device.addresses[0]);
+			if (!(deviceId in self.devices)) {
+				self.devices[deviceId] = device;
+
+				var dev_config = {
+					addresses: device.addresses,
+					name: self.getDeviceTxtValue(device, 'fn', device.host)
+				};
+				self.device = new Device(dev_config);
+				self.emit('deviceOn', self.device);
+			}
+		}
+	});
+
+	setTimeout(function() {
+		console.log('chromecast-js: device discovery stopped');
+		browser.stop();
+	}, discoverTimeout);
+};
--- tests/test.js
+++ tests/test.js
@@ -1,189 +1,194 @@
-chromecastjs = require('../')
-
-var browser = new chromecastjs.Browser()
+/**
+ * Test for all device calls.
+ * Recommended to be run with DEBUG=castv2 to see underlying protocol communication.
+ */
+chromecastjs = require('../');
 
 var media = {
-    url : 'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4',
-    subtitles: [{
-        language: 'en-US',
-        url: 'http://carlosguerrero.com/captions_styled.vtt',
-        name: 'English',
-    },
-    {
-        language: 'es-ES',
-        url: 'http://carlosguerrero.com/captions_styled_es.vtt',
-        name: 'Spanish',
-    }
-    ],
-    cover: {
-        title: 'Big Bug Bunny',
-        url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg'
-    },
-    subtitles_style: {
-          backgroundColor: '#FFFFFFFF', // see http://dev.w3.org/csswg/css-color/#hex-notation
-          foregroundColor: '#000FFFF', // see http://dev.w3.org/csswg/css-color/#hex-notation
-          edgeType: 'DROP_SHADOW', // can be: "NONE", "OUTLINE", "DROP_SHADOW", "RAISED", "DEPRESSED"
-          edgeColor: '#AA00FFFF', // see http://dev.w3.org/csswg/css-color/#hex-notation
-          fontScale: 1.5, // transforms into "font-size: " + (fontScale*100) +"%"
-          fontStyle: 'BOLD_ITALIC', // can be: "NORMAL", "BOLD", "BOLD_ITALIC", "ITALIC",
-          fontFamily: 'Droid Sans',
-          fontGenericFamily: 'CURSIVE', // can be: "SANS_SERIF", "MONOSPACED_SANS_SERIF", "SERIF", "MONOSPACED_SERIF", "CASUAL", "CURSIVE", "SMALL_CAPITALS",
-          windowColor: '#AA00FFFF', // see http://dev.w3.org/csswg/css-color/#hex-notation
-          windowRoundedCornerRadius: 10, // radius in px
-          windowType: 'ROUNDED_CORNERS' // can be: "NONE", "NORMAL", "ROUNDED_CORNERS"
-    }
+	url : 'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4',
+	subtitles: [
+		{
+			language: 'en-US',
+			url: 'http://carlosguerrero.com/captions_styled.vtt',
+			name: 'English'
+		},
+		{
+			language: 'es-ES',
+			url: 'http://carlosguerrero.com/captions_styled_es.vtt',
+			name: 'Spanish'
+		}
+	],
+	cover: {
+		title: 'Big Bug Bunny',
+		url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg'
+	},
+	subtitles_style: {
+		backgroundColor: '#FFFFFFFF', // see http://dev.w3.org/csswg/css-color/#hex-notation
+		foregroundColor: '#000FFFF', // see http://dev.w3.org/csswg/css-color/#hex-notation
+		edgeType: 'DROP_SHADOW', // can be: "NONE", "OUTLINE", "DROP_SHADOW", "RAISED", "DEPRESSED"
+		edgeColor: '#AA00FFFF', // see http://dev.w3.org/csswg/css-color/#hex-notation
+		fontScale: 1.5, // transforms into "font-size: " + (fontScale*100) +"%"
+		fontStyle: 'BOLD_ITALIC', // can be: "NORMAL", "BOLD", "BOLD_ITALIC", "ITALIC",
+		fontFamily: 'Droid Sans',
+		fontGenericFamily: 'CURSIVE', // can be: "SANS_SERIF", "MONOSPACED_SANS_SERIF", "SERIF", "MONOSPACED_SERIF", "CASUAL", "CURSIVE", "SMALL_CAPITALS",
+		windowColor: '#AA00FFFF', // see http://dev.w3.org/csswg/css-color/#hex-notation
+		windowRoundedCornerRadius: 10, // radius in px
+		windowType: 'ROUNDED_CORNERS' // can be: "NONE", "NORMAL", "ROUNDED_CORNERS"
+	}
+};
+
+var browser = new chromecastjs.Browser();
+
+browser.on('deviceOn', function(device) {
+	testDevice(device);
+});
+
+function testDevice(device) {
+	console.log('discovered chromecast: ', device.config.name);
+	console.log('connecting on %s', device.host);
+
+	// Starting to play Big Buck Bunny (made in Blender) exactly in the first minute without subtitles or cover.
+	// device.play('http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4', 60, function(){
+	// 	console.log('Playing in your chromecast!')
+	// });
+	device.play(media, 0, function() {
+		console.log('playing media on chromecast: ', media.url);
+
+		setTimeout(function() {
+			device.setVolume( 0.25, function( err, newVol){
+				if(err) console.error("error changing the volume");
+				else console.log('volume set to: '+newVol.level);
+			});
+		}, 5000);
+		
+		setTimeout(function() {
+			device.setVolumeMuted(true, function(err, newVol) {
+				if(err) console.error("error muting the volume");
+				else console.log('volume muted');
+			});
+		}, 10000);
+		
+		setTimeout(function() {
+			device.setVolumeMuted(false, function(err, newVol) {
+				if(err) console.error("error un-muting the volume");
+				else console.log('volume un-muted');
+			});
+		}, 15000);
+
+		setTimeout(function() {
+			device.setVolume( 0.5, function( err, newVol){
+				if(err) console.error("there was an error changing the volume");
+				else console.log('volume set to: '+newVol.level);
+			});
+		}, 20000);
+
+		setTimeout(function() {
+			device.subtitlesOff(function(err,status){
+				if(err) console.error("error setting subtitles off");
+				else console.log("subtitles off");
+			});
+		}, 25000);
+
+		setTimeout(function() {
+			device.changeSubtitles(1, function(err, status){
+				if(err) console.error("error restoring subtitles");
+				else console.log("subtitles on to Spanish");
+			});
+		}, 30000);
+
+		setTimeout(function() {
+			device.pause(function(){
+				console.log('paused!');
+			});
+		}, 35000);
+
+		setTimeout(function() {
+			device.unpause(function(){
+				console.log('unpaused!');
+			});
+		}, 40000);
+
+		setTimeout(function() {
+			device.changeSubtitles(0, function(err, status){
+				if(err) console.error("error restoring subtitles");
+				else console.log("subtitles changed back to English");
+			});
+		}, 45000);
+
+		setTimeout(function() {
+			device.changeSubtitlesSize(10, function(err, status){
+				if(err) console.error("error increasing subtitles size.");
+				else console.log("subtitles size increased");
+			});
+		}, 46000);
+
+		setTimeout(function() {
+			device.seek(30,function(){
+				console.log('seeking forward');
+			});
+		}, 50000);
+
+		setTimeout(function() {
+			device.changeSubtitlesSize(1, function(err, status){
+				if(err) console.error("error decreasing subtitles size.");
+				else console.log("subtitles size decreased");
+			});
+		}, 60000);
+
+		setTimeout(function() {
+			device.pause(function(){
+				console.log('paused!');
+			});
+		}, 70000);
+
+		setTimeout(function() {
+			device.seek(30,function(){
+				console.log('seeking forward');
+			});
+		}, 80000);
+
+		setTimeout(function() {
+			device.seek(30,function(){
+				console.log('seeking forward');
+			});
+		}, 85000);
+
+		setTimeout(function() {
+			device.unpause(function(){
+				console.log('unpaused!');
+			});
+		}, 90000);
+
+		setTimeout(function() {
+			device.seek(-30,function(){
+				console.log('seeking backwards');
+			});
+		}, 100000);
+
+		setTimeout(function() {
+			device.seekTo(0,function(){
+				console.log('seeking back to start');
+			});
+		}, 110000);
+		
+		setTimeout(function() {
+			device.seekTo(300,function(){
+				console.log('seeking to exactly 5 mins');
+			});
+		}, 120000);
+
+		setTimeout(function() {
+			device.getStatus(function(status) {
+				device.seekTo( status.media.duration - 60 ,function(){
+					console.log('seeking to 60 sec before end');
+				});
+			});
+		}, 130000);
+		
+		setTimeout(function() {
+			device.stop(function(){
+				console.log('stopped!');
+			});
+		}, 150000);
+	});
 }
-
-var media = {
-    //"url":"http://192.168.0.100:4009/5%20-%203%20-%20Lecture%205.3%20-%20Opportunity%20identification%20(29-59).mp4",
-    //url : 'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4',
-    url : "http://192.168.0.100:8899/6.11.mp4",
-    "subtitles":[{
-        "language":"en-US",
-        //"url":"http://192.168.0.100:9999/subtitles.vtt",
-
-        url: 'http://carlosguerrero.com/captions_styled.vtt',
-        "name":"Spanish"
-        }]
-    }
-
-var media = {"url":"http://192.168.0.100:8899/6.11.mp4","subtitles":[{"language":"en-US","url":"http://carlosguerrero.com/captions_styled.vtt","name":"Subtitle"}]}
-
-
-browser.on('deviceOn', function(device){
-  device.connect()
-  device.on('connected', function(){
-
-    // Starting to play Big Buck Bunny (made in Blender) exactly in the first minute without subtitles or cover.
-    //device.play('http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4', 60, function(){
-    //    console.log('Playing in your chromecast!')
-    //});
-
-    // Starting to play Big Buck Bunny (made in Blender) exactly in the first minute with example subtitles and cover.
-    device.play(media, 0, function(){
-        console.log('Playing in your chromecast!')
-
-        setTimeout(function(){
-            console.log("muting audio!")
-            device.setVolume( 0, function( err, newVol){
-                if(err) console.log("there was an error changing the volume.")
-                console.log('Volume Changed to: '+newVol.level)
-            });
-        }, 15000);
-
-        setTimeout(function(){
-            console.log('subtitles off!')
-            device.subtitlesOff(function(err,status){
-                if(err) console.log("error setting subtitles off...")
-                console.log("subtitles removed.")
-            });
-        }, 20000);
-
-        setTimeout(function(){
-            console.log("restoring audio!")
-            device.setVolume( 1, function( err, newVol){
-                if(err) console.log("there was an error changing the volume.")
-                console.log('Volume Changed to: '+newVol.level)
-            });
-        }, 21000);
-
-
-        setTimeout(function(){
-            console.log('subtitles on!')
-            device.changeSubtitles(1, function(err, status){
-                if(err) console.log("error restoring subtitles...")
-                console.log("subtitles restored.")
-            });
-        }, 25000);
-
-        setTimeout(function(){
-            console.log('subtitles on!')
-            device.changeSubtitles(1, function(err, status){
-                if(err) console.log("error restoring subtitles...")
-                console.log("subtitles restored.")
-            });
-        }, 25000);
-
-        setTimeout(function(){
-            device.pause(function(){
-                console.log('Paused!')
-            });
-        }, 30000);
-
-        setTimeout(function(){
-            device.unpause(function(){
-                console.log('unpaused!')
-            });
-        }, 40000);
-
-        setTimeout(function(){
-            console.log('I ment English subtitles!')
-            device.changeSubtitles(0, function(err, status){
-                if(err) console.log("error restoring subtitles...")
-                console.log("English subtitles restored.")
-            });
-        }, 45000);
-
-        setTimeout(function(){
-            console.log('Increasing subtitles size...')
-            device.changeSubtitlesSize(10, function(err, status){
-                if(err) console.log("error increasing subtitles size...")
-                console.log("subtitles size increased.")
-            });
-        }, 46000);
-
-        setTimeout(function(){
-            device.seek(30,function(){
-                console.log('seeking forward!')
-            });
-        }, 50000);
-
-        setTimeout(function(){
-            console.log('decreasing subtitles size...')
-            device.changeSubtitlesSize(1, function(err, status){
-                if(err) console.log("error...")
-                console.log("subtitles size decreased.")
-            });
-        }, 60000);
-
-        setTimeout(function(){
-            device.pause(function(){
-                console.log('Paused!')
-            });
-        }, 70000);
-
-        setTimeout(function(){
-            device.seek(30,function(){
-                console.log('seeking forward!')
-            });
-        }, 80000);
-
-        setTimeout(function(){
-            device.seek(30,function(){
-                console.log('seeking forward!')
-            });
-        }, 85000);
-
-        setTimeout(function(){
-            device.unpause(function(){
-                console.log('unpaused!')
-            });
-        }, 90000);
-
-
-        setTimeout(function(){
-            device.seek(-30,function(){
-                console.log('seeking backwards!')
-            });
-        }, 100000);
-
-
-        setTimeout(function(){
-            device.stop(function(){
-                console.log('Stoped!')
-            });
-        }, 200000);
-
-    })
-  })
-})
